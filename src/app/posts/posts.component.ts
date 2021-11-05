c48c93ca8dcf0a86d022804f1ccec39427cfc0c7
fumi238000 <s21231128@gmail.com>
2021-11-05 21:13:01 +0900

    データの受け渡しの実装

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
+  day = new Date()
+  posts = {
+    day: `${this.day.getMonth()}月 ${this.day.getDay()}日 `,
+    title: "test title",
+    details: "details",
+   }
 
   constructor() { }
 

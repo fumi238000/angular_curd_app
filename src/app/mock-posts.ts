237c57d1e51e017a97b95dc9b66412a0af673486
fumi238000 <s21231128@gmail.com>
2021-11-05 21:53:27 +0900

    データをオブジェクトから配列へ変更

--- src/app/mock-posts.ts
+++ src/app/mock-posts.ts
@@ -2,11 +2,37 @@
 
 import { Post } from "./post"
 
-export const POSTS: Post = {
-  id: 1,
-  day: "2020/12/31",
-  title: "test title",
-  details: "details",
-}
+export const POSTS: Post[] = [
+  {
+    id: 1,
+    day: "2020/12/31",
+    title: "test title1",
+    details: "details",
+  },
+  {
+    id: 2,
+    day: "2020/12/31",
+    title: "test title2",
+    details: "details",
+  },
+  {
+    id: 3,
+    day: "2020/12/31",
+    title: "test title3",
+    details: "details",
+  },
+  {
+    id: 4,
+    day: "2020/12/31",
+    title: "test title4",
+    details: "details",
+  },
+  {
+    id: 5,
+    day: "2020/12/31",
+    title: "test title4",
+    details: "details",
+  }
+]
   
   // day: `${day.getMonth()}月 ${day.getDay()}日 `,

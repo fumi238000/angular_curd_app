cc513b22b7ab21b0585d59b3ed5feea5fb9aeaad
fumi238000 <s21231128@gmail.com>
2021-11-09 20:01:42 +0900

    HttpClientのエラーハンドリングを実装 #6

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -3,6 +3,7 @@ import { Post } from './post';
 import { POSTS } from './mock-posts';
 import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { catchError, map, tap }from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -16,10 +17,21 @@ export class PostService {
 
   getPosts(): Observable<Post[]> {
     return this.http.get<Post[]>(this.postsUrl)
+      .pipe(
+        tap(posts => console.log(`投稿作成しました`)),
+        catchError(this.handleError<Post[]>('getPosts',[]))
+      )
   }
 
   //idを渡すと、そのidを持つ投稿データを１つ返す関数
   getPost(id: number): Observable<Post | undefined> {
     return of(POSTS.find(post => post.id === id));
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      
+      return of(result as T)
+    }
   }
 }

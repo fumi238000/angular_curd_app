c10d298bce220c509e42a6833f3c1fd0565e0545
fumi238000 <s21231128@gmail.com>
2021-11-13 09:25:24 +0900

    投稿を更新するメソッドを実装 #7

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Post } from './post';
 import { POSTS } from './mock-posts';
 import { Observable, of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, tap }from 'rxjs/operators'
 
 @Injectable({
@@ -10,6 +10,9 @@ import { catchError, map, tap }from 'rxjs/operators'
 })
 export class PostService {
   private postsUrl = 'api/posts';
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   constructor(
     private http: HttpClient
@@ -32,6 +35,14 @@ export class PostService {
         tap(posts => console.log(`投稿データを取得しました`)),
         catchError(this.handleError<Post>(`getPost id={id}`))
       );
+  }
+
+  updatePost(post: Post): Observable<any> {
+    return this.http.put(this.postsUrl, post, this.httpOptions)
+      .pipe(
+        tap(_ => console.log(`投稿データを更新しました`)),
+        catchError(this.handleError<any>('updatePost'))
+      )
   }
 
   private handleError<T>(operation = 'operation', result?: T) {
